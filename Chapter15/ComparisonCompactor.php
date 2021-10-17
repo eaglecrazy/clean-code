c341d6210ec6daf9416eaf26d4e545de98de870a
eaglezzzzz <eaglecrazy@mail.ru>
2021-10-17 11:33:33 +0300

    Chapter 15 - "JUnit Internals". Remove "f" prefix. Refactor compact method.

--- Chapter15/ComparisonCompactor.php
+++ Chapter15/ComparisonCompactor.php
@@ -10,43 +10,40 @@ class ComparisonCompactor
     const DELTA_END = "]";
     const DELTA_START = "[";
 
-    private int $fContextLength;
-    private string $fExpected;
-    private string $fActual;
-    private int $fPrefix;
-    private int $fSuffix;
+    private int $contextLength;
+    private string $expected;
+    private string $actual;
+    private string $compactExpected;
+    private string $compactActual;
+    private int $prefix;
+    private int $suffix;
 
     public function __construct(int $contextLength, string $expected, string $actual)
     {
-        $this->fContextLength = $contextLength;
-        $this->fExpected = $expected;
-        $this->fActual = $actual;
+        $this->contextLength = $contextLength;
+        $this->expected = $expected;
+        $this->actual = $actual;
     }
 
-    public function compact(string $message): string
+    public function formatCompactedComparision(string $message): string
     {
-        if ($this->fExpected === '' || $this->fActual === '' || $this->areStringsEqual()) {
-            return $this->format($message, $this->fExpected, $this->fActual);
+        if ($this->canBeCompacted()) {
+            $this->compactExpectedAndActual();
+
+            return $this->format($message, $this->compactExpected, $this->compactActual);
         }
 
-        $this->findCommonPrefix();
-        $this->findCommonSuffix();
-
-        $expected = $this->compactString($this->fExpected);
-
-        $actual = $this->compactString($this->fActual);
-
-        return $this->format($message, $expected, $actual);
+        return $this->format($message, $this->expected, $this->actual);
     }
 
     private function compactString(string $source): string
     {
-        $result = self::DELTA_START . substring($source, $this->fPrefix, strlen($source) - $this->fSuffix + 1) . self::DELTA_END;
+        $result = self::DELTA_START . substring($source, $this->prefix, strlen($source) - $this->suffix + 1) . self::DELTA_END;
 
-        if ($this->fPrefix > 0)
+        if ($this->prefix > 0)
             $result = $this->computeCommonPrefix() . $result;
 
-        if ($this->fSuffix > 0)
+        if ($this->suffix > 0)
             $result = $result . $this->computeCommonSuffix();
 
         return $result;
@@ -54,52 +51,52 @@ class ComparisonCompactor
 
     private function findCommonPrefix(): void
     {
-        $end = min(strlen($this->fExpected), strlen($this->fActual));
+        $end = min(strlen($this->expected), strlen($this->actual));
 
-        for ($this->fPrefix = 0; $this->fPrefix < $end; $this->fPrefix++) {
-            if ($this->fExpected[$this->fPrefix] != $this->fActual[$this->fPrefix])
+        for ($this->prefix = 0; $this->prefix < $end; $this->prefix++) {
+            if ($this->expected[$this->prefix] != $this->actual[$this->prefix])
                 break;
         }
     }
 
     private function findCommonSuffix(): void
     {
-        $expectedSuffix = strlen($this->fExpected) - 1;
-        $actualSuffix = strlen($this->fActual) - 1;
+        $expectedSuffix = strlen($this->expected) - 1;
+        $actualSuffix = strlen($this->actual) - 1;
 
-        for (; $actualSuffix >= $this->fPrefix && $expectedSuffix >= $this->fPrefix; $actualSuffix--, $expectedSuffix--) {
-            if ($this->fExpected[$expectedSuffix] != $this->fActual[$actualSuffix])
+        for (; $actualSuffix >= $this->prefix && $expectedSuffix >= $this->prefix; $actualSuffix--, $expectedSuffix--) {
+            if ($this->expected[$expectedSuffix] != $this->actual[$actualSuffix])
                 break;
         }
 
-        $this->fSuffix = strlen($this->fExpected) - $expectedSuffix;
+        $this->suffix = strlen($this->expected) - $expectedSuffix;
     }
 
     private function computeCommonPrefix(): string
     {
-        return ($this->fPrefix > $this->fContextLength ? self::ELLIPSIS : "")
+        return ($this->prefix > $this->contextLength ? self::ELLIPSIS : "")
             . substring(
-                $this->fExpected,
-                max(0, $this->fPrefix - $this->fContextLength),
-                $this->fPrefix
+                $this->expected,
+                max(0, $this->prefix - $this->contextLength),
+                $this->prefix
             );
     }
 
     private function computeCommonSuffix(): string
     {
-        $end = min(strlen($this->fExpected) - $this->fSuffix + 1 + $this->fContextLength, strlen($this->fExpected));
+        $end = min(strlen($this->expected) - $this->suffix + 1 + $this->contextLength, strlen($this->expected));
 
         return substring(
-                $this->fExpected,
-                strlen($this->fExpected) - $this->fSuffix + 1,
+                $this->expected,
+                strlen($this->expected) - $this->suffix + 1,
                 $end)
-            . (strlen($this->fExpected) - $this->fSuffix + 1 < strlen($this->fExpected) - $this->fContextLength ? static::ELLIPSIS : "");
+            . (strlen($this->expected) - $this->suffix + 1 < strlen($this->expected) - $this->contextLength ? static::ELLIPSIS : "");
 
     }
 
     private function areStringsEqual(): bool
     {
-        return $this->fExpected === $this->fActual;
+        return $this->expected === $this->actual;
     }
 
     private function format(string $message, string $expected, string $actual): string
@@ -107,5 +104,20 @@ class ComparisonCompactor
         $message = $message ? $message . ' ' : '';
 
         return $message . 'expected:<' . $expected . '> but was:<' . $actual . '>';
+    }
+
+    private function canBeCompacted(): bool
+    {
+        return $this->expected != '' && $this->actual != '' && !$this->areStringsEqual();
+    }
+
+    private function compactExpectedAndActual(): void
+    {
+        $this->findCommonPrefix();
+        $this->findCommonSuffix();
+
+        $this->compactExpected = $this->compactString($this->expected);
+
+        $this->compactActual = $this->compactString($this->actual);
     }
 }
--- tests/Chapter15/ComparisonCompactorTest.php
+++ tests/Chapter15/ComparisonCompactorTest.php
@@ -9,117 +9,117 @@ class ComparisonCompactorTest extends TestCase
 {
     public function testMessage()
     {
-        $failure = (new ComparisonCompactor(0, "b", "c"))->compact("a");
+        $failure = (new ComparisonCompactor(0, "b", "c"))->formatCompactedComparision("a");
         self::assertTrue("a expected:<[b]> but was:<[c]>" === $failure);
     }
 
     public function testStartSame()
     {
-        $failure = (new ComparisonCompactor(1, "ba", "bc"))->compact('');
+        $failure = (new ComparisonCompactor(1, "ba", "bc"))->formatCompactedComparision('');
         echo 'failure = "' . $failure . '"' . PHP_EOL;
         self::assertEquals("expected:<b[a]> but was:<b[c]>", $failure);
     }
 
     public function testEndSame()
     {
-        $failure = (new ComparisonCompactor(1, "ab", "cb"))->compact('');
+        $failure = (new ComparisonCompactor(1, "ab", "cb"))->formatCompactedComparision('');
         self::assertEquals("expected:<[a]b> but was:<[c]b>", $failure);
     }
 
     public function testSame()
     {
-        $failure = (new ComparisonCompactor(1, "ab", "ab"))->compact('');
+        $failure = (new ComparisonCompactor(1, "ab", "ab"))->formatCompactedComparision('');
         self::assertEquals("expected:<ab> but was:<ab>", $failure);
     }
 
     public function testNoContextStartAndEndSame()
     {
-        $failure = (new ComparisonCompactor(0, "abc", "adc"))->compact('');
+        $failure = (new ComparisonCompactor(0, "abc", "adc"))->formatCompactedComparision('');
         self::assertEquals("expected:<...[b]...> but was:<...[d]...>", $failure);
     }
 
     public function testStartAndEndContext()
     {
-        $failure = (new ComparisonCompactor(1, "abc", "adc"))->compact('');
+        $failure = (new ComparisonCompactor(1, "abc", "adc"))->formatCompactedComparision('');
         self::assertEquals("expected:<a[b]c> but was:<a[d]c>", $failure);
     }
 
     public function testStartAndEndContextWithEllipses()
     {
-        $failure = (new ComparisonCompactor(1, "abcde", "abfde"))->compact('');
+        $failure = (new ComparisonCompactor(1, "abcde", "abfde"))->formatCompactedComparision('');
         self::assertEquals("expected:<...b[c]d...> but was:<...b[f]d...>", $failure);
     }
 
     public function testComparisonErrorStartSameComplete()
     {
-        $failure = (new ComparisonCompactor(2, "ab", "abc"))->compact('');
+        $failure = (new ComparisonCompactor(2, "ab", "abc"))->formatCompactedComparision('');
         self::assertEquals("expected:<ab[]> but was:<ab[c]>", $failure);
     }
 
     public function testComparisonErrorEndSameComplete()
     {
-        $failure = (new ComparisonCompactor(0, "bc", "abc"))->compact('');
+        $failure = (new ComparisonCompactor(0, "bc", "abc"))->formatCompactedComparision('');
         echo $failure . PHP_EOL;
         self::assertEquals("expected:<[]...> but was:<[a]...>", $failure);
     }
 
     public function testComparisonErrorEndSameCompleteContext()
     {
-        $failure = (new ComparisonCompactor(2, "bc", "abc"))->compact('');
+        $failure = (new ComparisonCompactor(2, "bc", "abc"))->formatCompactedComparision('');
         self::assertEquals("expected:<[]bc> but was:<[a]bc>", $failure);
     }
 
     public function testComparisonErrorOverlapingMatches()
     {
-        $failure = (new ComparisonCompactor(0, "abc", "abbc"))->compact('');
+        $failure = (new ComparisonCompactor(0, "abc", "abbc"))->formatCompactedComparision('');
         self::assertEquals("expected:<...[]...> but was:<...[b]...>", $failure);
     }
 
     public function testComparisonErrorOverlapingMatchesContext()
     {
-        $failure = (new ComparisonCompactor(2, "abc", "abbc"))->compact('');
+        $failure = (new ComparisonCompactor(2, "abc", "abbc"))->formatCompactedComparision('');
         self::assertEquals("expected:<ab[]c> but was:<ab[b]c>", $failure);
     }
 
     public function testComparisonErrorOverlapingMatches2()
     {
-        $failure = (new ComparisonCompactor(0, "abcdde", "abcde"))->compact('');
+        $failure = (new ComparisonCompactor(0, "abcdde", "abcde"))->formatCompactedComparision('');
         self::assertEquals("expected:<...[d]...> but was:<...[]...>", $failure);
     }
 
     public function testComparisonErrorOverlapingMatches2Context()
     {
-        $failure = (new ComparisonCompactor(2, "abcdde", "abcde"))->compact('');
+        $failure = (new ComparisonCompactor(2, "abcdde", "abcde"))->formatCompactedComparision('');
         self::assertEquals("expected:<...cd[d]e> but was:<...cd[]e>", $failure);
     }
 
     public function testComparisonErrorWithActualNull()
     {
-        $failure = (new ComparisonCompactor(0, "a", ''))->compact('');
+        $failure = (new ComparisonCompactor(0, "a", ''))->formatCompactedComparision('');
         self::assertEquals("expected:<a> but was:<>", $failure);
     }
 
     public function testComparisonErrorWithActualNullContext()
     {
-        $failure = (new ComparisonCompactor(2, "a", ''))->compact('');
+        $failure = (new ComparisonCompactor(2, "a", ''))->formatCompactedComparision('');
         self::assertEquals("expected:<a> but was:<>", $failure);
     }
 
     public function testComparisonErrorWithExpectedNull()
     {
-        $failure = (new ComparisonCompactor(0, '', "a"))->compact('');
+        $failure = (new ComparisonCompactor(0, '', "a"))->formatCompactedComparision('');
         self::assertEquals("expected:<> but was:<a>", $failure);
     }
 
     public function testComparisonErrorWithExpectedNullContext()
     {
-        $failure = (new ComparisonCompactor(2, '', "a"))->compact('');
+        $failure = (new ComparisonCompactor(2, '', "a"))->formatCompactedComparision('');
         self::assertEquals("expected:<> but was:<a>", $failure);
     }
 
     public function testBug609972()
     {
-        $failure = (new ComparisonCompactor(10, "S&P500", "0"))->compact('');
+        $failure = (new ComparisonCompactor(10, "S&P500", "0"))->formatCompactedComparision('');
         self::assertEquals("expected:<[S&P50]0> but was:<[]0>", $failure);
     }
 }
