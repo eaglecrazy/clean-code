d55278d8b37a2c524feddb51e596b8b4ae44232c
eaglecrazy <munoyoo3munoyoo3>
2021-10-23 14:49:09 +0300

    Chapter 15 - "JUnit Internals". Rearrange methods.

--- Chapter15/ComparisonCompactor.php
+++ Chapter15/ComparisonCompactor.php
@@ -32,43 +32,10 @@ class ComparisonCompactor
             $this->findCommonPrefixAndSuffix();
 
             $compactExpected = $this->compact($this->expected);
-            $compactActual   = $this->compact($this->actual);
+            $compactActual = $this->compact($this->actual);
         }
 
         return $this->format($message, $compactExpected, $compactActual);
-    }
-
-    private function compact(string $s): string
-    {
-        return $this->startingEllipsis()
-            . $this->startingContext()
-            . self::DELTA_START
-            . $this->delta($s)
-            . self::DELTA_END
-            . $this->endingContext()
-            . $this->endingEllipsis();
-    }
-
-    private function findCommonPrefix(): void
-    {
-        $end = min(strlen($this->expected), strlen($this->actual));
-
-        for ($this->prefixLength = 0; $this->prefixLength < $end; $this->prefixLength++) {
-            if ($this->expected[$this->prefixLength] != $this->actual[$this->prefixLength])
-                break;
-        }
-    }
-
-    private function areStringsEqual(): bool
-    {
-        return $this->expected === $this->actual;
-    }
-
-    private function format(string $message, string $expected, string $actual): string
-    {
-        $message = $message ? $message . ' ' : '';
-
-        return $message . 'expected:<' . $expected . '> but was:<' . $actual . '>';
     }
 
     private function shouldBeCompacted(): bool
@@ -81,6 +48,11 @@ class ComparisonCompactor
         return $this->expected === ''
             || $this->actual === ''
             || $this->areStringsEqual();
+    }
+
+    private function areStringsEqual(): bool
+    {
+        return $this->expected === $this->actual;
     }
 
     private function findCommonPrefixAndSuffix(): void
@@ -105,6 +77,27 @@ class ComparisonCompactor
     {
         return strlen($this->actual) - $this->suffixLength <= $this->prefixLength
             || strlen($this->expected) - $this->suffixLength <= $this->prefixLength;
+    }
+
+    private function findCommonPrefix(): void
+    {
+        $end = min(strlen($this->expected), strlen($this->actual));
+
+        for ($this->prefixLength = 0; $this->prefixLength < $end; $this->prefixLength++) {
+            if ($this->expected[$this->prefixLength] != $this->actual[$this->prefixLength])
+                break;
+        }
+    }
+
+    private function compact(string $s): string
+    {
+        return $this->startingEllipsis()
+            . $this->startingContext()
+            . self::DELTA_START
+            . $this->delta($s)
+            . self::DELTA_END
+            . $this->endingContext()
+            . $this->endingEllipsis();
     }
 
     private function startingEllipsis(): string
@@ -139,5 +132,12 @@ class ComparisonCompactor
     private function endingEllipsis(): string
     {
         return $this->suffixLength > $this->contextLength ? self::ELLIPSIS : '';
+    }
+
+    private function format(string $message, string $expected, string $actual): string
+    {
+        $message = $message ? $message . ' ' : '';
+
+        return $message . 'expected:<' . $expected . '> but was:<' . $actual . '>';
     }
 }
